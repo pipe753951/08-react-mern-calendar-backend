b92f2b9a0d490369deb81305bce8bf595f06942d
David Puentes <pipe01112009@gmail.com>
2026-05-23 21:31:14 -0500

    Finishing lesson #363, making index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("¡Hola mundo!");
+import express from "express";
+
+// Crear servidor Express
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(`Un sistema solicitó la ruta ${request.url}`);
+  response.json({
+    ok: true,
+  });
+});
+
+// Escuchar peticiones
+app.listen(4000, () => {
+  console.log("Servidor corriendo en el puerto 4000.");
+});
