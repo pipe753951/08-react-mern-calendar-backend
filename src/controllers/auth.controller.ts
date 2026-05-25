257f192cb599eaa701477174953d8e165ab7d76a
David Puentes <pipe01112009@gmail.com>
2026-05-24 19:12:17 -0500

    Finishing lesson #373, implementing UserModel that uses UserSchema to use it with database, and implementing it to registerUser controller.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express";
 import type { RegisterUserRequestBody } from "../types/requests/auth/RegisterUserRequestBody.interface";
 import type { LoginUserRequestBody } from "../types/requests/auth/LoginUserRequestBody.interface";
 
+import { UserModel } from "../models/User.model";
+
 const loginUser = (request: Request, response: Response): Response => {
   const requestBody: LoginUserRequestBody = request.body;
 
@@ -15,14 +17,34 @@ const loginUser = (request: Request, response: Response): Response => {
   });
 };
 
-const registerUser = (request: Request, response: Response): Response => {
+const registerUser = async (
+  request: Request,
+  response: Response,
+): Promise<Response> => {
   const requestBody: RegisterUserRequestBody = request.body;
 
-  return response.json({
-    ok: true,
-    routeType: "register",
-    ...requestBody,
-  });
+  try {
+    const user = new UserModel(requestBody);
+    await user.save();
+
+    return response.status(200).json({
+      ok: true,
+      routeType: "register",
+      ...requestBody,
+    });
+  } catch (error) {
+    console.error(
+      new Error(
+        "Hubo un error en la operación de guardar un nuevo usuario en la base de datos",
+        { cause: error },
+      ),
+    );
+    return response.status(500).json({
+      ok: false,
+      message:
+        "Hubo un error interno desde el servidor. Se recomienda que te comuniques con el administrador para encontrar una solución.",
+    });
+  }
 };
 
 const renewUserAuthToken = (request: Request, response: Response): Response => {
