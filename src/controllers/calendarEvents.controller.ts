3b1f72478a3d3e6683bce6ee9066ef31d04be6fb
David Puentes <pipe01112009@gmail.com>
2026-05-26 12:08:07 -0500

    Finishing task from lesson #382 and #383.

---
+++ src/controllers/calendarEvents.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+
+const getCalendarEvents = async (
+  request: Request,
+  response: Response,
+): Promise<Response> => {
+  console.log(`Petición a ${request.url}`);
+
+  return response.json({
+    ok: true,
+    controller: "getEvents",
+  });
+};
+
+const createCalendarEvent = async (
+  request: Request,
+  response: Response,
+): Promise<Response> => {
+  console.log(`Petición a ${request.url}`);
+
+  return response.json({
+    ok: true,
+    controller: "createCalendarEvents",
+  });
+};
+
+const updateCalendarEvent = async (
+  request: Request,
+  response: Response,
+): Promise<Response> => {
+  console.log(`Petición a ${request.url}`);
+
+  return response.json({
+    ok: true,
+    controller: "updateCalendarEvents",
+  });
+};
+
+const deleteCalendarEvent = async (
+  request: Request,
+  response: Response,
+): Promise<Response> => {
+  console.log(`Petición a ${request.url}`);
+
+  return response.json({
+    ok: true,
+    controller: "deleteCalendarEvents",
+  });
+};
+
+export {
+  getCalendarEvents,
+  createCalendarEvent,
+  updateCalendarEvent,
+  deleteCalendarEvent,
+};
---
+++ src/routes/calendarEvents.router.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+import validateJwt from "../middlewares/validateJwt.middleware";
+import {
+  createCalendarEvent,
+  deleteCalendarEvent,
+  getCalendarEvents,
+  updateCalendarEvent,
+} from "../controllers/calendarEvents.controller";
+
+const calendarEventsRouter = Router();
+
+calendarEventsRouter.use(validateJwt);
+
+calendarEventsRouter.get("/", getCalendarEvents);
+calendarEventsRouter.post("/", createCalendarEvent);
+calendarEventsRouter.put("/:id", updateCalendarEvent);
+calendarEventsRouter.delete("/:id", deleteCalendarEvent);
